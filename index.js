af3168acb0136da1f970ec15a6ffaeb3fe44363d
JrzenonDev <jrobertoonb@gmail.com>
2019-11-19 21:28:30 -0300

    async await

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyPaser = require('body-parser');
+const { promisefy } = require('util');
+
 const GoogleSpreadsheet = require('google-spreadsheet');
 const credentials = require('./bugtracker.json');
 
@@ -22,23 +24,21 @@ app.get('/', (request, response) => {
     response.render('home');
 });
 
-app.post('/', (request, response) => {
-
-    // id da planilha do drive
-    const doc =  new GoogleSpreadsheet(docId);
-    doc.useServiceAccountAuth(credentials, (err) => {
-        if (err) {
-            console.log('Não foi possível abrir a planilha');
-        } else {
-            console.log('Planilha aberta');
-            doc.getInfo((err, info) => {
-                const worksheet = info.worksheets[worksheetIndex];
-                worksheet.addRow({ name: request.body.name, email: request.body.email, classificacao: request.body.issueType, como_reproduzir_erro: request.body.howToReproduce, saida_esperada: request.body.expectedOutput, saida_recebida: request.body.receivedOutput }, err => {
-                    response.send('bug reportado com sucesso!');
-                })
-            })
-        }
-    })
+app.post('/', async(request, response) => {
+    try{
+        const doc = new GoogleSpreadSheet(docId)
+        await promisify(doc.useServiceAccountAuth)(credentials)
+        const info = await promisify(doc.getInfo)()
+        const worksheet = info.worksheets[worksheetIndex]
+        await promisify(worksheet.addRow)({
+            name: request.body.name,
+            email: request.body.email
+        })
+        response.send('bug reportado com sucesso')
+    } catch (err) {
+        response.send('erro ao enviar formulário')
+        console.log(err)
+    }
 })
 
 app.listen(3000, err => {
--- testar-planilha.js
+++ testar-planilha.js
@@ -1,18 +1,32 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
 const credentials = require('./bugtracker.json');
+const { promisify } = require('util');
+
+const addRowToSheet = async() => {
+    const doc =  new GoogleSpreadsheet('1QvsTZ5YEfSjgoUVD5_ci7AY5hs5CUtopWkeobkrJehA');
+    await promisify(doc.useServiceAccountAuth)(credentials);
+    console.log('planilha aberta');
+    const info = await promisify(doc.getInfo)();
+    const worksheet = info.worksheets[0];
+    await promisify(worksheet.addRow)({ name: 'José' , email: 'teste@teste.com' });
+}
+
+addRowToSheet();
 
 // id da planilha do drive
-const doc =  new GoogleSpreadsheet('1QvsTZ5YEfSjgoUVD5_ci7AY5hs5CUtopWkeobkrJehA');
-doc.useServiceAccountAuth(credentials, (err) => {
-    if (err) {
-        console.log('Não foi possível abrir a planilha');
-    } else {
-        console.log('Planilha aberta');
-        doc.getInfo((err, info) => {
-            const worksheet = info.worksheets[0];
-            worksheet.addRow({ name: 'José' , email: 'teste@teste.com' }, err => {
-                console.log('linha inserida');
-            });
-        })
-    }
-})
+// const doc =  new GoogleSpreadsheet('1QvsTZ5YEfSjgoUVD5_ci7AY5hs5CUtopWkeobkrJehA');
+// doc.useServiceAccountAuth(credentials, (err) => {
+//     if (err) {
+//         console.log('Não foi possível abrir a planilha');
+//     } else {
+//         console.log('Planilha aberta');
+//         doc.getInfo((err, info) => {
+//             const worksheet = info.worksheets[0];
+//             worksheet.addRow({ name: 'José' , email: 'teste@teste.com' }, err => {
+//                 console.log('linha inserida');
+//             });
+//         })
+//     }
+// })
+
+
